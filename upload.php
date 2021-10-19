ad911984b6241220fcfcbbfc4d63a0b63b57184a
robng98 <91499233+robng98@users.noreply.github.com>
2021-10-19 01:54:43 -0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,5 +1,15 @@
-<?php
+<!-- <?php
 
 $targetPath = "uploads/" . basename($_FILES["inpFile"]["name"]);
 move_uploaded_file($_FILES["inpFile"]["tmp_name"], $targetPath);
 
+ -->
+   
+<?php
+
+var_dump($_FILES);
+
+foreach($_FILES["myFiles"]["tmp_name"] as $key => $value){
+    $targetPath = "uploads/" . basename($_FILES["myFiles"]["name"][$key]);
+    move_uploaded_file($value, $targetPath);
+}
